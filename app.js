796a2bcf6875f4d312f6062bb72adceb79a38d4b
Danesh Tolani <danesh.tolani@gmail.com>
2023-04-13 11:36:16 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+// GLOBALS - NO WINDOW !!!
+// __dirname - path to current directory
+// __filename - filename
+// require - function to use modules (CommonJs)
+// module - info about current
+// process - info about env where the program is being executed
