47cbf20771ac77e9f7bf49ca1fd316c5f544cfe8
Danesh Tolani <danesh.tolani@gmail.com>
2023-04-13 13:08:52 +0530

    learned os

--- app.js
+++ app.js
@@ -4,17 +4,3 @@
 // require - function to use modules (CommonJs)
 // module - info about current
 // process - info about env where the program is being
-
-// CommonJs EVERY FILE IN NODE IS A MODULE
-// Modules - Encapsulated Code (only share minimum)
-
-const names = require("./4-firstModule");
-const sayHi = require("./5-utils");
-const data = require("./6-alternativeExports");
-require("./7-mind-grenade"); // this is running the file "7-mind-grenade"
-
-// console.log(data);
-
-// sayHi("Danesh");
-// sayHi(names.john);
-// sayHi(names.peter);
