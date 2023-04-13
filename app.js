b12b1b2be8ce1a86d348d877f14381f3a0ca0870
Danesh Tolani <danesh.tolani@gmail.com>
2023-04-13 11:44:30 +0530

    learned globals

--- app.js
+++ app.js
@@ -3,4 +3,9 @@
 // __filename - filename
 // require - function to use modules (CommonJs)
 // module - info about current
-// process - info about env where the program is being executed
+// process - info about env where the program is being
+
+// console.log(process.env);
+setInterval(() => {
+  console.log("Hello");
+}, 1000);
