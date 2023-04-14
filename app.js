94252182ad3281eaaf84f83265e4bb3c47219e20
Danesh Tolani <danesh.tolani@gmail.com>
2023-04-14 11:09:59 +0530

    eventloop exercise

--- app.js
+++ app.js
@@ -15,6 +15,10 @@
 // npm install -g <packageName>
 // sudo install -g <packageName> (mac)
 
+// dev dependency
+// npm install <packageName> -D
+// npm install <packageName> --save-dev
+
 //  package.json - manifest file (stores info about project/packages)
 // manual approach (create package.json in root)
 // npm init
@@ -25,4 +29,4 @@ const _ = require("lodash");
 const items = [1, [2, [3, [4]]]];
 
 const newItems = _.flattenDeep(items);
-console.log(newItems);
+console.log("Hello there");
