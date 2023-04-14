d705ad71c356ca892cff306deabb905267061134
Danesh Tolani <danesh.tolani@gmail.com>
2023-04-14 08:05:22 +0530

    created package.json

--- app.js
+++ app.js
@@ -14,3 +14,15 @@
 // global dependency - use it in any project
 // npm install -g <packageName>
 // sudo install -g <packageName> (mac)
+
+//  package.json - manifest file (stores info about project/packages)
+// manual approach (create package.json in root)
+// npm init
+// npm init -y (everything default)
+
+const _ = require("lodash");
+
+const items = [1, [2, [3, [4]]]];
+
+const newItems = _.flattenDeep(items);
+console.log(newItems);
